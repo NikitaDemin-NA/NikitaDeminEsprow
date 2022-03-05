45029735a488f0865c1b8ed9f0b01aeedc8298ac
«Никита <asdcfg98@gmail.com>
2022-03-05 13:41:04 +0300

    refactoring

--- Report1/report.js
+++ Report1/report.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/main/resources/features/1%20-%20E2E%20check%20Subscription.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/main/resources/features/2%20-%20Add%20exchange.feature");
 formatter.feature({
-  "name": "E2E check Subscription of ETP Markets",
+  "name": "Check adding Exchange subscription of ETP Markets",
   "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@E2E"
+      "name": "@Add"
     },
     {
       "name": "@all"
@@ -48,12 +48,12 @@ formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Delete, add, pay, modify Exchange subscription",
+  "name": "Add Exchange subscription",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@E2E"
+      "name": "@Add"
     },
     {
       "name": "@all"
@@ -91,11 +91,11 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "add 2 extra Sessions for all Exchanges",
+  "name": "add 2 new Exchange with \"FIX 4.4\" Protocol Type and 2 Number of Sessions",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "steps.CustomSteps.addExtraSessions(int)"
+  "location": "steps.CustomSteps.AddNewExchange(int,java.lang.String,int)"
 });
 formatter.result({
   "status": "passed"
@@ -118,8 +118,38 @@ formatter.match({
   "location": "steps.CustomSteps.payForTheExchange()"
 });
 formatter.result({
-  "error_message": "java.lang.Exception: value of \"Pay Now\" button is not correct\n\tat steps.UICustomSteps.checkPayNowOrder(UICustomSteps.java:339)\n\tat steps.CustomSteps.payForTheExchange(CustomSteps.java:149)\n\tat ✽.pay for the Exchanges(file:///Users/andrei/IdeaProjects/NikitaDeminEsprow/src/main/resources/features/1%20-%20E2E%20check%20Subscription.feature:30)\n",
+  "error_message": "java.lang.Exception: value of \"Pay Now\" button is not correct\n\tat steps.UICustomSteps.checkPayNowOrder(UICustomSteps.java:339)\n\tat steps.CustomSteps.payForTheExchange(CustomSteps.java:149)\n\tat ✽.pay for the Exchanges(file:///Users/andrei/IdeaProjects/NikitaDeminEsprow/src/main/resources/features/2%20-%20Add%20exchange.feature:28)\n",
   "status": "failed"
+});
+formatter.step({
+  "name": "add 3 new Exchange with \"FIX 5.0\" Protocol Type and 3 Number of Sessions",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "steps.CustomSteps.AddNewExchange(int,java.lang.String,int)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "check Final Account",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "steps.CustomSteps.checkFinalAccount()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "pay for the Exchanges",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "steps.CustomSteps.payForTheExchange()"
+});
+formatter.result({
+  "status": "skipped"
 });
 formatter.step({
   "name": "close browser",
