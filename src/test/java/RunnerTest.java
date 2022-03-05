306c232d37f64939e03cfd8fc295be4baed2c334
«Никита <asdcfg98@gmail.com>
2022-03-05 13:41:04 +0300

    refactoring

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.Cucumber;
         plugin = {"pretty", "html:Report1", "json:target/cucumber.json"},
         features = "src/main/resources/features",
         glue = "steps",
-        tags = "@E2E"
+        tags = "@Add"
 )
 public class RunnerTest {
 }
